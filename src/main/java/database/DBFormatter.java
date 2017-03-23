d4c2e3548d45ba555a5ecab95fccd8b9b41065b8
Reinierstam <reinierstam@gmail.com>
2017-03-23 11:22:58 +0100

    updated formatter

--- src/main/java/database/DBFormatter.java
+++ src/main/java/database/DBFormatter.java
@@ -15,18 +15,19 @@ import java.util.TimeZone;
  */
 public class DBFormatter {
     private DBConnector connector = DBConnector.getInstance();
-    public void formatDB(){
+    public void formatDB(String timeframe, String newTimeframe){
         SensorsRoute sensorRoute = new SensorsRoute();
         JSONObject allSensors = sensorRoute.GetAllSensors();
+        formatCollection("name", timeframe, newTimeframe);
     }
 
-    private void  formatCollection(String collectionName){
+    private void  formatCollection(String collectionName, String timeframe, String newTimeframe){
         BasicDBObject whereQuery = new BasicDBObject();
-        whereQuery.put("timeframe", "frame");
-        DateFormat df=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSZZ");
-        df.setTimeZone(TimeZone.getTimeZone("UTC"));
-        String currentDate = df.format(new Date(System.currentTimeMillis() - 3600 * 1000));
-        whereQuery.put("timestamp", new BasicDBObject("$gt",currentDate));
+        whereQuery.put("timeframe", timeframe);
+        String currentDate = GetDate(timeframe, true);
+        String lastDate = GetDate(newTimeframe, false);
+        whereQuery.put("timestamp", new BasicDBObject("$lt",currentDate));
+        whereQuery.put("timestamp", new BasicDBObject("$gt",lastDate));
         JSONArray period = connector.findQuery(collectionName,whereQuery);
         int total = 0;
         for (int i = 0; i < period.size(); i++) {
@@ -36,7 +37,31 @@ public class DBFormatter {
         BasicDBObject newDoc = new BasicDBObject();
         newDoc.put("timestamp",currentDate);
         newDoc.put("value",total/period.size());
-        newDoc.put("timeframe","hour");
+        newDoc.put("timeframe",newTimeframe);
         connector.insert(newDoc,collectionName);
+    }
+
+    private String GetDate(String timeframe, boolean current){
+        String format = "yyyy-MM-dd'T'00:00:00.00ZZ";
+        int HoursAdjustment = 0;
+        if (timeframe == "frame") {
+            format = "yyyy-MM-dd'T'HH:mm:ss.SSZZ";
+        }
+        if(!current){
+            switch (timeframe){
+                case "hour":
+                    HoursAdjustment = 1;
+                    break;
+                case "day":
+                    HoursAdjustment = 24;
+                    break;
+                case "week" :
+                    HoursAdjustment = 168;
+                    break;
+            }
+        }
+        DateFormat df=new SimpleDateFormat(format);
+        df.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return df.format(new Date(System.currentTimeMillis() - 3600 * 1000 * HoursAdjustment));
     }
 }
