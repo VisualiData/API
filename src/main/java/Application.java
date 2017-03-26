b725aa072ac8e152934ac12b3a49a9330cdac600
Arko Rietdijk <arko.rietdijk@gmail.com>
2017-03-26 18:50:19 +0200

    Added custom 404 and 500

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,22 +1,25 @@
-/**
- * Created by Gebruiker on 13-3-2017.
+/*
+ * Main class for initializing API
  */
 
 import api.*;
 import database.DBConnector;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import util.ResponseUtil;
 
-import static spark.Spark.after;
-import static spark.Spark.before;
+import static spark.Spark.*;
 
 public class Application{
     private static final Logger LOGGER = LogManager.getLogger(Application.class);
+    private Application(){}
+
     public static void main (String[] args){
+        init();
         start();
     }
 
-    public static void start(){
+    private static void start(){
         DBConnector.initDB();
         IURL[] addUrls = {
                 new AddSensorUrl(),
@@ -25,17 +28,20 @@ public class Application{
                 new GetAllSensorsUrl(),
                 new GetSensorDataUrl(),
                 new PostSensorDataUrl(),
-                new FormatDataBaseUrl()
-        };
+                new FormatDataBaseUrl(),
+                new GetSensorURL()
+            };
         for (IURL url : addUrls){
             url.openUrl();
         }
+        errorHandling();
         before((request, response) -> LOGGER.debug(request.requestMethod() + " request made to: " + request.uri()));
         after((request, response) -> response.type("application/json"));
     }
 
-    public void stop(){
-        stop();
-        DBConnector.close();
+
+    private static void errorHandling(){
+        notFound(ResponseUtil.generateFailed("Not Found", 404).toJSONString());
+        internalServerError(ResponseUtil.generateFailed("Internal server error", 500).toJSONString());
     }
 }
