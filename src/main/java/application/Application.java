fb77109a385629f2b34bfc9da305678ff71d8a85
Arko Rietdijk <arko.rietdijk@gmail.com>
2017-05-22 10:48:42 +0200

    JS api calls now work

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -43,13 +43,30 @@ public class Application{
             url.openUrl();
         }
         errorHandling();
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
         before((request, response) -> {
             LOGGER.debug(request.requestMethod() + " request made to: " + request.uri());
+            LOGGER.debug(request.body());
             if(DBConnector.getDBState() == DatabaseState.STATE_RUNNING){
-                if(!authenticated(request) && !("/sensordata/dummy".equals(request.uri()) || "/sensordata".equals(request.uri()))){
-                    response.type("application/json");
-                    halt(ResponseCodes.NOT_AUTHORIZED, ResponseUtil.generateFailed("Not authorized", ResponseCodes.NOT_AUTHORIZED).toJSONString());
-                }
+                System.out.println("request made");
+//                if ()
+//                if(!authenticated(request) && !("/sensordata/dummy".equals(request.uri()) || "/sensordata".equals(request.uri())) ){
+//                    response.type("application/json");
+//                    halt(ResponseCodes.NOT_AUTHORIZED, ResponseUtil.generateFailed("Not authorized", ResponseCodes.NOT_AUTHORIZED).toJSONString());
+//                }
             } else {
                 DBQuery.checkDBUp();
                 halt(ResponseCodes.SERVER_ERROR, ResponseUtil.generateFailed("DB down", ResponseCodes.SERVER_ERROR).toJSONString());
